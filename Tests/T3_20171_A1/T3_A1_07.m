b6a133e03aae0602e39dccf35f4143c6745613f1
Felipe Tormes <fbtormes@inf.ufrgs.br>
2017-12-12 13:26:07 -0200

    Add solutions of test 3.

---
+++ Tests/T3_20171_A1/T3_A1_07.m
@@ -0,0 +1,17 @@
+﻿disp("Q7");
+
+function u=euler_mod(h,Tmax)  
+  y= 1.4;  
+  itmax = Tmax/h;  
+ 
+  for n=1:itmax  
+    t=(n-1)*h;  
+    k1 = ((4*t)/y- t*y);  
+    y_til = y +h*k1;  
+    k2 = ((4*(t+h))/y_til - (t+h)*y_til);  
+    y = y + h * (k1 + k2)/2; 
+  endfor
+  
+  printf("%1.6e\n",y);
+  
+endfunction
