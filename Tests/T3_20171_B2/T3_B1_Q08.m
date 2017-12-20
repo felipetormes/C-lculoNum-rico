742250cfd89e672931aada12c37f1ac30f03a410
Felipe Tormes <felipe.tormes@gmail.com>
2017-12-19 23:24:11 -0200

    Update question 8 of test B2.

--- Tests/T3_20171_B2/T3_B1_Q08.m
+++ Tests/T3_20171_B2/T3_B1_Q08.m
@@ -2,43 +2,23 @@ format long
 
 f = @(y,t) (sin(3*t) - (2*t*y)) / (t.^2);
 
-function [y,t]=euler_mod(f,t0,y0,h,n)  
-  y = zeros(n+1,1); 
-  t = zeros(n+1,1);
-  y_til = zeros(n+1,1);
-  
-  y(1) = y0;
-  t(1) = t0;
- 
-  for n=1:n
-    t(n+1) = t(n) + h; 
-    y_til(n+1) = y(n) + h*f(y(n),t(n)); 
-    y(n+1) = y(n) + (h/2) * (f(y(n),t(n)) + f(y_til(n+1),t(n+1)));
-  endfor
-endfunction
+h = 0.02;
+n = 45;
+y = zeros(n+1,1); 
+t = zeros(n+1,1);
+y_til = zeros(n+1,1);
+y(1) = 2;
+t(1) = 1.1;
 
-[yeuler,t] = euler_mod(f,1.1,2,0.02,3);
-[yeuler,t]
+for i=1:3
+  t(i+1) = t(i) + h; 
+  y_til(i+1) = y(i) + h*f(y(i),t(i)); 
+  y(i+1) = y(i) + (h/2) * (f(y(i),t(i)) + f(y_til(i+1),t(i+1)));
+endfor
 
-function [y,t] = ab4(f, t0, t1, t2, t3, y0, y1, y2, y3, h, n) #Adam-Bashforth de 3º Ordem
-    y = zeros(n+1, 1);
-    t = zeros(n+1, 1);
-    
-    t(1) = t0;
-    t(2) = t1;
-    t(3) = t2;
-    t(4) = t3;
-    y(1) = y0;
-    y(2) = y1;
-    y(3) = y2;
-    y(4) = y3;
-    
-    
-    for n = 4:n
-        t(n+1) = t(n) + h;
-        y(n+1) = y(n) + h*(55*f(y(n),t(n))-59*f(y(n-1),t(n-1))+37*f(y(n-2),t(n-2))-9*f(y(n-3),t(n-3)))/24;
-    endfor
-endfunction
+for i = 4:n
+  t(i+1) = t(i) + h;
+  y(i+1) = y(i) + h*(55*f(y(i),t(i))-59*f(y(i-1),t(i-1))+37*f(y(i-2),t(i-2))-9*f(y(i-3),t(i-3)))/24;
+endfor
 
-[yab4,t] = ab4(f, t(1), t(2), t(3), t(4), yeuler(1), yeuler(2), yeuler(3), yeuler(4), 0.02, 45);
-[yab4,t]
+y(n+1)
